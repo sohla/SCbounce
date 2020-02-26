d2ba36780bd12d795047c0bdaa65f8fcf714e478
soh_la <soh_la@yahoo.com>
2020-02-26 14:43:51 +1100

    cleaned up code

--- embed/WemosBN0055/src/main.cpp
+++ embed/WemosBN0055/src/main.cpp
@@ -3,41 +3,17 @@
 #include <Adafruit_BNO055.h>
 #include <utility/imumaths.h>
 
-/* This driver uses the Adafruit unified sensor library (Adafruit_Sensor),
-   which provides a common 'type' for sensor data and some helper functions.
-   To use this driver you will also need to download the Adafruit_Sensor
-   library and include it in your libraries folder.
-   You should also assign a unique ID to this sensor for use with
-   the Adafruit Sensor API so that you can identify this particular
-   sensor in any data logs, etc.  To assign a unique ID, simply
-   provide an appropriate value in the constructor below (12345
-   is used by default in this example).
-   Connections
-   ===========
-   Connect SCL to analog 5
-   Connect SDA to analog 4
-   Connect VDD to 3.3-5V DC
-   Connect GROUND to common ground
-   History
-   =======
-   2015/MAR/03  - First release (KTOWN)
-*/
-
-/* Set the delay between fresh samples */
+//--------------------------------------------------------------------------
 #define BNO055_SAMPLERATE_DELAY_MS (100)
 
-// Check I2C device address and correct line below (by default address is 0x29 or 0x28)
-//                                   id, address
+//--------------------------------------------------------------------------
 Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x29);
 
-/**************************************************************************/
-/*
-    Displays some basic information on this sensor from the unified
-    sensor API sensor_t type (see Adafruit_Sensor for more information)
-*/
-/**************************************************************************/
-void displaySensorDetails(void)
-{
+//--------------------------------------------------------------------------
+//
+//--------------------------------------------------------------------------
+void displaySensorDetails(void){
+
   sensor_t sensor;
   bno.getSensor(&sensor);
   Serial.println("------------------------------------");
@@ -52,39 +28,32 @@ void displaySensorDetails(void)
   delay(500);
 }
 
-/**************************************************************************/
-/*
-    Arduino setup function (automatically called at startup)
-*/
-/**************************************************************************/
-void setup(void)
-{
-  Serial.begin(115200);//115200
-  Serial.println("Orientation Sensor Test"); Serial.println("");
+//--------------------------------------------------------------------------
+//
+//--------------------------------------------------------------------------
+void setup(void) {
 
-  /* Initialise the sensor */
-  if(!bno.begin())
-  {
-    /* There was a problem detecting the BNO055 ... check your connections */
+  Serial.begin(115200);
+
+
+
+
+  // init BNO055
+  if(!bno.begin()) {
     Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
     while(1);
   }
    
   delay(1000);
-
-  /* Use external crystal for better accuracy */
   bno.setExtCrystalUse(true);
-   
-  /* Display some basic information on this sensor */
   displaySensorDetails();
+
 }
 
-/**************************************************************************/
-/*
-    Arduino loop function, called once 'setup' is complete (your own code
-    should go here)
-*/
-/**************************************************************************/
+//--------------------------------------------------------------------------
+//
+//--------------------------------------------------------------------------
+
 void loop(void){
 
   sensors_event_t event;
