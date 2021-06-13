d13140985167c1f55ccd88cffbb17d84c000cc44
Steph OHara <soh_la@yahoo.com>
2021-06-13 21:57:29 +1000

    update network to new SOHLA3 netcomm router

--- embed/m5stickc/src/main.cpp
+++ embed/m5stickc/src/main.cpp
@@ -20,9 +20,9 @@
 // const char *password = "sohla3letmein";  //LAN password
 // const IPAddress outIp(192,168,20,11);  //LAN address
 
-const char *ssid = "nukuNet"; //LAN name
-const char *password = "zxzxzxzx";  //LAN password
-const IPAddress outIp(10,1,1,8);  //LAN address
+const char *ssid = "SOHLA3"; //LAN name
+const char *password = "sohla3letmein";  //LAN password
+const IPAddress outIp(192,168,20,10);  //LAN address
 
 //--------------------------------------------------------------------------
 
@@ -45,14 +45,14 @@ bool IMU6886Flag = false;
 
 //--------------------------------------------------------------------------
 // // Set your Static IP address
-// IPAddress local_IP(192,168,20,STATIP);
+IPAddress local_IP(192,168,20,STATIP);
 // // Set your Gateway IP address
-// IPAddress gateway(192,168,20,254);
+IPAddress gateway(192,168,20,254);
 
 // // Set your Static IP address
-IPAddress local_IP(10,1,1,STATIP);
+// IPAddress local_IP(10,1,1,STATIP);
 // Set your Gateway IP address
-IPAddress gateway(10,1,1,254);
+// IPAddress gateway(10,1,1,254);
 
 
 IPAddress subnet(255, 255, 255, 0);
