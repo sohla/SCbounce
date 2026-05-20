cbfb97ee4c1615cc684d6272697b273255ca3d7e
steph <stephohara0@gmail.com>
2026-05-20 20:38:43 +1000

    removing old old files

--- sc_osx_standalone-3.7.0-template/run.sh
+++
@@ -1,9 +0,0 @@
-echo "includePaths:
-    - overwrites
-    - SCClassLibrary
-excludePaths:
-    - $HOME/Library/Application Support/SuperCollider/Extensions
-    - /Library/Application Support/SuperCollider/Extensions
-    - /Applications/SuperCollider3.9.1/SuperCollider.app/Contents/Resources/SCClassLibrary
-postInlineWarnings: true" > langconf.yaml;
-./MacOS/sclang -a -l langconf.yaml init.scd
